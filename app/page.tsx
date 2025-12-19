588fe29eee5e5b16fa987d1770bdb6aa1d442542
l200240229 <l200240229@student.ums.ac.id>
2025-12-19 15:31:19 +0700

    .

--- app/page.tsx
+++ app/page.tsx
@@ -116,7 +116,7 @@ export default function PublicDashboard() {
             Daftar Talenta
           </h3>
           <Link
-            href="/api/profiles/public/talents/${username}/"
+            href="/api/profiles/public/talents/"
             className="inline- text-center bg-blue-600 text-sm text-white-600 rounded hover:bg-blue-700"
           >
             View All →
